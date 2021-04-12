8f9fd63008be7eb94675e0579c5a18d70561dca0
magnuen2k <maen030@student.kristiania.no>
2021-04-12 23:02:47 +0200

    make auth branch

--- src/test/java/org/quizgame/LocalApplicationRunner.java
+++ src/test/java/org/quizgame/LocalApplicationRunner.java
@@ -2,6 +2,7 @@ package org.quizgame;
 
 import org.springframework.boot.SpringApplication;
 
+//
 public class LocalApplicationRunner {
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
